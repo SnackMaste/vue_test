b967daa48fcb6bdd73ccdf96135e4d9b2630e78a
SnackMaste <jcgarcia1565@misena.edu.co>
2024-08-17 12:47:55 -0500

    feat: :sparkles: Add new compoponent header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useAuthStore } from "../store/authStore";
+
+const authStore = useAuthStore();
+const name = authStore.name;
+const checkout = authStore.checkOut;
+</script>
+<template>
+  <div
+    class="w-full flex border-b border-yellow-400 justify-center items-center h-40 text-center text-yellow-400"
+  >
+    <div class="w-1/4"></div>
+    <div class="w-1/2">
+      <h1 class="text-3xl font-bold">Bienvenid@ {{ name }}</h1>
+    </div>
+    <div class="w-1/4">
+      <button v-on:click="checkout" class="font-semibold">Salir</button>
+    </div>
+  </div>
+</template>
