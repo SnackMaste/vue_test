e49df8ad48877f56a06a1342e57d8f251f9f0903
SnackMaste <jcgarcia1565@misena.edu.co>
2024-08-17 12:46:41 -0500

    feat: :sparkles: Add header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import { RouterView, useRoute } from "vue-router";
+import HeaderComponent from "./components/HeaderComponent.vue";
+
+const route = useRoute();
 </script>
 
 <template>
+  <HeaderComponent v-if="route.path !== '/'" />
   <router-view></router-view>
 </template>
-
