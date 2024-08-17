6ffa53f27cd42397854b02bb19e1664b7a18f098
SnackMaste <jcgarcia1565@misena.edu.co>
2024-08-17 12:51:56 -0500

    feat: :lock: Add security config
    se agrego un metodo de seguridad pare redirigir al usuario si ya ah iniciado que lo lleve de una vez a product

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,24 +1,33 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import { authGuard } from './authGuard';
-import HomePage from '../view/HomePage.vue';
-import ProductPage from '../view/ProductPage.vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { authGuard } from "./authGuard";
+import { useAuthStore } from "../store/authStore";
+import HomePage from "../view/HomePage.vue";
+import ProductPage from "../view/ProductPage.vue";
 
 const publicRoutes: RouteRecordRaw[] = [
-  { path: '/', component: HomePage },
+  {
+    path: "/",
+    component: HomePage,
+    beforeEnter: (to, from, next) => {
+      const authStore = useAuthStore();
+      if (authStore.name) {
+        next("/product");
+      } else {
+        next();
+      }
+    },
+  },
 ];
 
 const privateRoutes: RouteRecordRaw[] = [
-  { path: '/product', component: ProductPage, meta: { requiresAuth: true } }
+  { path: "/product", component: ProductPage, meta: { requiresAuth: true } },
 ];
 
-const routes: RouteRecordRaw[] = [
-  ...publicRoutes,
-  ...privateRoutes
-];
+const routes: RouteRecordRaw[] = [...publicRoutes, ...privateRoutes];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
+  routes,
 });
 
 router.beforeEach(authGuard);
