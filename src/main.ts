e6c0f54b8c2e5a2ff802c57395cb50530c1dcc98
SnackMaste <jcgarcia1565@misena.edu.co>
2024-08-17 12:47:32 -0500

    feat: :sparkles: Add pinia plugin persist

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
-import './style.css'
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './routers/router';
-import { createPinia } from 'pinia';
+import "./style.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./routers/router";
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 const pinia = createPinia();
 const app = createApp(App);
+pinia.use(piniaPluginPersistedstate);
 
 app.use(pinia);
 app.use(router);
-app.mount('#app');
-
+app.mount("#app");
