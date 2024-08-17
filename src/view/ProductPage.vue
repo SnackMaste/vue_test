5a77c6f05d169593539c3c78653e3bb7264ed261
SnackMaste <jcgarcia1565@misena.edu.co>
2024-08-17 12:53:19 -0500

    feat: :sparkles: send props for ProductCard

--- src/view/ProductPage.vue
+++ src/view/ProductPage.vue
@@ -1,9 +1,14 @@
-<script setup>
-import ProductCard from '../components/cards/ProductCard.vue';
+<script setup type="ts">
+import ProductCard from "../components/cards/ProductCard.vue";
+import { useAuthStore } from "../store/authStore";
+
+const authStore = useAuthStore();
+
+const color = authStore.color;
+const name = authStore.name;
 </script>
 <template>
-    <div class="flex flex-wrap justify-center w-full">
-        <ProductCard />
-    </div>
-    
+  <div class="flex flex-wrap justify-center w-full">
+    <ProductCard :color="color" :name="name" />
+  </div>
 </template>
