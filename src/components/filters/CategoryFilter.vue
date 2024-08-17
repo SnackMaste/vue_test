09e64ac1878b2789696b26a110d2bb376b259301
SnackMaste <jcgarcia1565@misena.edu.co>
2024-08-17 12:53:49 -0500

    feat: :sparkles: Add files base

---
+++ src/components/filters/CategoryFilter.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full flex justify-center items-center">
+    <div class="w-1/2 flex flex-col justify-center items-center">
+      <select></select>
+    </div>
+  </div>
+</template>
