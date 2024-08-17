125bf0aa2170a6409c417d8ecc648b5fd0b6c17c
SnackMaste <jcgarcia1565@misena.edu.co>
2024-08-17 12:45:30 -0500

    style: :beers: changes for format

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,11 +1,10 @@
-declare module '*.vue' {
-    import { DefineComponent } from 'vue';
-    const component: DefineComponent<{}, {}, any>;
-    export default component;
-  }
-  
-  declare module './routers/router' {
-    const router: any;
-    export default router;
-  }
-  
+declare module "*.vue" {
+  import { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
+
+declare module "./routers/router" {
+  const router: any;
+  export default router;
+}
