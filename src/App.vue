e76873b4c0917a65dece1936615b6fdfbea645d1
SnackMaste <jcgarcia1565@misena.edu.co>
2024-08-18 00:12:48 -0500

    feat: :sparkles: Add contex
    se agrego un contexto para que los demas componentes pudieran acceder a los datos globales de los productos

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
 import { RouterView, useRoute } from "vue-router";
 import HeaderComponent from "./components/HeaderComponent.vue";
+import ProductContext from "./components/context/ProductContext.vue";
 
 const route = useRoute();
 </script>
 
 <template>
-  <HeaderComponent v-if="route.path !== '/'" />
-  <router-view></router-view>
+  <div class="bg-gray-950 w-full min-h-screen">
+    <HeaderComponent v-if="route.path !== '/'" />
+    <ProductContext>
+      <router-view></router-view>
+    </ProductContext>
+  </div>
 </template>
---
+++ src/components/context/ProductContext.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { getProducts } from "@/services/apiService";
+import { provide, ref, onMounted } from "vue";
+
+const products = ref(null);
+
+onMounted(async () => {
+  products.value = await getProducts();
+});
+
+provide("products", products);
+</script>
+
+<template>
+  <slot></slot>
+</template>
