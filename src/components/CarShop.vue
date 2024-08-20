c1286522d4871bd53d8c23f83d013d734c9b3c4e
SnackMaste <jcgarcia1565@misena.edu.co>
2024-08-19 21:59:56 -0500

    fix: :bug: area de click
    se cambio las etiquetas y se ajusto el area de activida o click para un mejor funcionamiento

--- src/components/CarShop.vue
+++ src/components/CarShop.vue
@@ -13,21 +13,21 @@ defineProps({
 });
 </script>
 <template>
-  <div>
+  <button class="flex flex-col items-center justify-center h-34">
     <div
-      class="w-8 h-8 rounded-full bg-gray-950 border-2 border-yellow-400 relative top-7 left-12 z-10 text-center text-yellow-400"
+      class="w-8 h-8 rounded-full bg-gray-950 border-2 border-yellow-400 relative left-7 z-10 text-center text-yellow-400"
     >
       {{ counter }}
     </div>
-    <button
-      class="w-20 h-20 rounded-full bg-gray-950 border-2 border-yellow-400 flex justify-center items-center"
-    >
-      <ShoppingCart class="text-yellow-400 w-12 h-12 relative" />
-    </button>
     <div
-      class="w-28 h-10 border-2 border-yellow-400 bg-gray-950 relative -top-3 right-3 text-center pt-1 text-yellow-400"
+      class="w-20 h-20 rounded-full bg-gray-950 border-2 border-yellow-400 flex justify-center items-center absolute"
+    >
+      <ShoppingCart class="text-yellow-400 w-12 h-12" />
+    </div>
+    <div
+      class="w-28 h-10 border-2 border-yellow-400 bg-gray-950 relative top-7 text-center pt-1 text-yellow-400"
     >
       {{ formatCurrency(precio) }}
     </div>
-  </div>
+  </button>
 </template>
