3d89ed800be7ede3a5aa80a5d440b3f2d3dfe11c
SnackMaste <jcgarcia1565@misena.edu.co>
2024-08-19 22:01:23 -0500

    refactor: :sparkles: Add AGREGAR button function
    se agrego la funcionalidad del boton de agregar producto, se emitio el valor y se agrego un componente para representar la cantidad en la card

--- src/components/cards/ProductCard.vue
+++ src/components/cards/ProductCard.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import { defineProps, computed } from "vue";
+import { defineProps, computed, ref, watch } from "vue";
 import Button from "../ui/button/Button.vue";
 import { formatCurrency } from "@/lib/utils";
+import {
+  NumberField,
+  NumberFieldContent,
+  NumberFieldDecrement,
+  NumberFieldIncrement,
+  NumberFieldInput,
+} from "@/components/ui/number-field";
 
+const cantidad = ref(0);
 const props = defineProps<{
+  id: string;
   color: string;
   image: string;
   name: string;
@@ -11,6 +20,8 @@ const props = defineProps<{
   category: string;
   detalleClick: () => void;
 }>();
+const idProducto = ref(props.id);
+const emit = defineEmits(["update:cantidad"]);
 
 function isColorLight(color: string): boolean {
   const hex = color.replace("#", "");
@@ -29,6 +40,18 @@ const textClass = computed(() =>
 const borderClass = computed(() =>
   isColorLight(props.color) ? "border-black" : "border-yellow-400",
 );
+
+const addProduct = () => {
+  cantidad.value = 1;
+};
+
+watch(cantidad, (nuevoValor) => {
+  emit("update:cantidad", {
+    id: props.id,
+    cantidad: nuevoValor,
+    precio: props.price,
+  });
+});
 </script>
 
 <template>
@@ -36,6 +59,7 @@ const borderClass = computed(() =>
     class="w-52 m-5 rounded-3xl border-2 min-h-80 flex flex-col items-center justify-around"
     :class="borderClass"
     :style="{ backgroundColor: props.color }"
+    :id="idProducto"
   >
     <img
       :src="`./src/assets/images/products/${props.image}`"
@@ -53,12 +77,31 @@ const borderClass = computed(() =>
           DETALLE
         </Button>
         <Button
+          v-if="cantidad === 0"
           variant="ghost"
           class="w-1/2 text-center border-t border-l py-2 font-semibold hover:text-base hover:px-1.5 hover:bg-transparent rounded-none"
           :class="[textClass, borderClass]"
+          @click="addProduct"
         >
           AGREGAR
         </Button>
+        <NumberField
+          v-else
+          class="w-1/2 rounded-none border border-b-0 focus:ring-offset-0 h-10"
+          :class="[textClass, borderClass]"
+          id="cantidad"
+          v-model="cantidad"
+          :default-value="1"
+          :min="0"
+        >
+          <NumberFieldContent>
+            <NumberFieldDecrement />
+            <NumberFieldInput
+              class="bg-transparent w-full rounded-none border-none focus-visible:ring-offset-0"
+            />
+            <NumberFieldIncrement />
+          </NumberFieldContent>
+        </NumberField>
       </div>
       <div
         class="w-full text-center py-2 text-wrap font-bold border-y-2 cursor-default"
